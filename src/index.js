95a6ac62b52ca64b6b14b0947c63484d7e142c6e
Aliya83311 <hamidullina833@gmail.com>
2021-02-11 16:20:48 +0700

    init: start typical arrays problems task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,24 @@
+exports.min = function min(array) {
+    if (array == false || array == undefined) {
+        return 0;
+    }
+    let sortedArr = array.sort((x, y) => x - y);
+    return sortedArr[0];
+};
 
-exports.min = function min (array) {
-  return 0;
-}
+exports.max = function max(array) {
+    if (array == false || array == undefined) {
+        return 0;
+    }
+    let sortedArr = array.sort((x, y) => x - y);
+    return sortedArr[sortedArr.length - 1];
+};
 
-exports.max = function max (array) {
-  return 0;
-}
-
-exports.avg = function avg (array) {
-  return 0;
-}
+exports.avg = function avg(array) {
+  if (array == false || array == undefined) {
+    return 0;
+  }
+  let counter = 0;
+  let newArray = array.map((value) => counter += value);
+  return counter / array.length;
+};
